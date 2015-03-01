c74032cbd03a047d0d623b38b34b61a25dcc00dd
MIDHUN <midhunadarvin@gmail.com>
2015-03-01 20:49:11 +0530

    Add database for login

--- app/data/destroy_session.php
+++ app/data/destroy_session.php
@@ -1,6 +1,12 @@
 <?php 
-	session_id('uid');
+/*	session_id('uid');
 	session_start();
 	session_destroy();
-	session_commit();
+	session_commit();   */
+
+	$db = new DbHandler();
+    $session = $db->destroySession();
+    $response["status"] = "info";
+    $response["message"] = "Logged out successfully";
+    echoResponse(200, $response);
 ?>
--- app/js/app.js
+++ app/js/app.js
@@ -35,6 +35,38 @@ app.factory('sessionService', ['$http', function($http){
     };
 }]);
 
+app.factory('Data', ['$http',function ($http, toaster) { 
+
+// This service connects to our REST API
+
+        var serviceBase = 'api/v1/';
+
+        var obj = {};
+
+        obj.get = function (q) {
+            return $http.get(serviceBase + q).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.post = function (q, object) {
+            return $http.post(serviceBase + q, object).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.put = function (q, object) {
+            return $http.put(serviceBase + q, object).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.delete = function (q) {
+            return $http.delete(serviceBase + q).then(function (results) {
+                return results.data;
+            });
+        };
+
+        return obj;
+}]);
+
 app.factory('loginService',function($http, $location, sessionService){
     return{
         login:function(data,scope){
