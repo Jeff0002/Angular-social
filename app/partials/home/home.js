4a1cd9ef2117055998432f89e6133cd0fbe24fc3
midhunadarvin <midhunadarvin@gmail.com>
2015-03-20 22:20:39 +0530

    home page nav

--- app/partials/home/home.js
+++ app/partials/home/home.js
@@ -1,5 +1,19 @@
 'use strict';
 
+
+/*var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+				content = document.getElementById('content');
+
+ $(".menu-toggle").click(function(e) {
+        e.preventDefault();
+        $('#content').toggleClass('cbp-spmenu-push-toright' );
+        $('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
+        classie.toggle( content, 'cbp-spmenu-push-toright' );
+		classie.toggle( menuLeft, 'cbp-spmenu-open' );
+ });*/
+
+	
+
 var app = angular.module('myApp.home', ['ngRoute'])
 
 
@@ -11,12 +25,18 @@ app.controller('homeCtrl', ['$scope','$location','Data',function ($scope,$locati
             $location.path('login');
         });
     }
-}])
+}]);
 
 /* Directives */
 
-.directive('homeSidebar',function(){
+app.directive('homeSidebar',function(){
 	return{
 		templateUrl: 'partials/home/sidebar.html'
+	}
+});
+
+app.directive('homeNavbar',function(){
+	return{
+		templateUrl: 'partials/home/navbar.html'
 	}
 });
